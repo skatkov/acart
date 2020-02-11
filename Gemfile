d63277bb64189834acf9e0322125e809c65d7ef4
Stanislav K <lunaticman@gmail.com>
2020-02-12 02:19:39 +0400

    enable erb and hotrealoads

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 ruby "2.7.0"
 
 gem 'sinatra'
+gem 'sinatra-contrib', '~> 2.0', '>= 2.0.8.1'
--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
+require "sinatra/reloader" if development?
 
 get '/' do
-  "Hello World!"
+  erb :index
 end
 
