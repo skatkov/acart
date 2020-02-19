cc856be81bac205e127fdeffab7c448b7eb95a35
Stanislav K <lunaticman@gmail.com>
2020-02-19 17:09:02 +0400

    force SSL on production

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ ruby "2.7.0"
 
 gem 'sinatra'
 gem 'sinatra-contrib', '~> 2.0', '>= 2.0.8.1'
+gem 'rack-ssl-enforcer'
 group :development, :test do
 	gem 'pry'
 end
--- app.rb
+++ app.rb
@@ -1,6 +1,11 @@
 require 'sinatra'
 require "sinatra/reloader" if development?
 require 'pry' if development?
+require 'rack/ssl-enforcer'
+
+configure do
+  use Rack::SslEnforcer if ENV['FORCE_SSL']
+end
 
 get '/' do
 	erb :index
