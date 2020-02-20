b66eadd992bf468b091eb7d9909cd5f3f6c62242
Stanislav K <lunaticman@gmail.com>
2020-02-20 23:26:33 +0400

    writting tests to cover

--- test/app_test.rb
+++ test/app_test.rb
@@ -12,13 +12,23 @@ class MyTest < MiniTest::Test
     assert last_response.ok?
   end
 
+  def test_redirect
+    id = Link.insert(url: 'https://amazon.com/dp/test')
+    get "/#{id}"
+    assert last_response.redirect?
+    assert_equal 'https://amazon.com/dp/test', last_response.location
+  end
+
   def test_shortener
+    cnt = Link.count
     post "/", {"items" =>
                  {"0" => {"asin" => "B07K8CZ5WT", "quantity" => "1"},
                   "1" => {"asin" => "B07QYXD2TH", "quantity" => "1"},},
                "associate_tag" => "",
                "locale" => "US",}
 
-    assert last_response.redirect?
+    refute last_response.redirect?
+    assert last_response.ok?
+    assert_equal(cnt+1, Link.count)
   end
 end
