6725a953e5a92c6f3d70a961aa200b6802bb0110
Stanislav (Stas) Katkov <sk@skylup.com>
2020-02-21 22:21:27 +0400

    Redesign (#11)
    * better looking warning, separate done.erb
    
    * Change done page to look better

--- src/index.js
+++ src/index.js
@@ -8,3 +8,6 @@ import { definitionsFromContext } from "stimulus/webpack-helpers"
 const application = Application.start()
 const context = require.context("./controllers", true, /\.js$/)
 application.load(definitionsFromContext(context))
+
+import { Clipboard } from "stimulus-form-utilities"
+application.register('clipboard', Clipboard)
